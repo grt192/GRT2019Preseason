d7c56ad68a678e3afda1908dbb79cc8bbfb46103
coolpeepz <colin.r.schultz@gmail.com>
2018-09-19 19:11:09 -0700

    updated NavX Hz t match swerve loop

--- src/main/java/frc/swerve/NavXGyro.java
+++ src/main/java/frc/swerve/NavXGyro.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.interfaces.Gyro;
 public class NavXGyro extends AHRS implements Gyro {
 
 	public NavXGyro() {
-		super(SPI.Port.kMXP);
+		super(SPI.Port.kMXP, (byte) 100);
 	}
 
 	@Override
