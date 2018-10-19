b4dfd6769dcec61ac7e954a642a35f98421e1ace
Audrey Xie <aaxxie13@gmail.com>
2018-10-19 15:28:53 -0700

    added some spaces

--- src/main/java/frc/hand-cv/obj-detect.py
+++ src/main/java/frc/hand-cv/obj-detect.py
@@ -16,19 +16,24 @@ while(cap.isOpened()):
     grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
     value = (35, 35)
     blurred = cv2.GaussianBlur(grey, value, 0)
+
     _, thresh1 = cv2.threshold(blurred, 127, 255,
                                cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
     cv2.imshow('Thresholded', thresh1)
+
     image, contours, hierarchy = cv2.findContours(thresh1.copy(), \
            cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     cnt = max(contours, key = lambda x: cv2.contourArea(x))
     x, y, w, h = cv2.boundingRect(cnt)
     cv2.rectangle(crop_img, (x, y), (x+w, y+h), (0, 0, 255), 0)
     M = cv2.moments(cnt)
+
     center_x = int(M['m10']/M['m00'])
     center_y = int(M['m01']/M['m00'])
+
     diff_x = old_x - center_x
     diff_y = old_y - center_y
+
     if mag < 0.2:
         mag = 0
         horiz = "none"
@@ -38,10 +43,12 @@ while(cap.isOpened()):
         horiz = "right" if (diff_x > 0) else "left"
         vert = "up" if (diff_y > 0) else "down"
         mag = round(((diff_x ** 2) + (diff_y ** 2) ** (0.5)), 2)
+
     if diff_x == 0 or diff_y == 0:
         angle = 0
     else:
         angle = np.arctan(diff_y / diff_x)
+
     angle = np.arctan(diff_y / diff_x)
     cv2.circle(crop_img, (center_x, center_y), 5, (255, 255, 255), -1)
 
